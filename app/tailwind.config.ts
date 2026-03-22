9bcaf3d2ac4ad96aae697fbdaff396958dff8edc
HaticeSenaSes <haticesena.ses@std.yeditepe.edu.tr>
2026-03-22 20:15:06 +0300

    Update tailwind.config.ts

--- app/tailwind.config.ts
+++ app/tailwind.config.ts
@@ -1,19 +1,12 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ['var(--font-geist-sans)', 'sans-serif'],
-        mono: ['var(--font-geist-mono)', 'monospace'],
-      },
-    },
+    extend: {},
   },
-  plugins: [require("tailwindcss-animate")],
-};
-export default config;
+  plugins: [],
+}
